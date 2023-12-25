69172911d6d8c669d1ac7e3ce2b3abd4c6b7e2c9
KenooG <seba123.2003@wp.pl>
2023-12-26 00:08:16 +0100

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState} from "react";
 
 function App() {
+
+    const [value, setValue] = useState('')
+
+    function handleInput(){
+
+
+    }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    <h1>todos</h1>
+        <div>
+            <input type="text" value={value} placeholder='What needs to be done?'/>
+        </div>
     </div>
   );
 }
