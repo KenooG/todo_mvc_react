5596d4d9449bb0da216ad2cb1a7a58e57e0d5cf6
KenooG <seba123.2003@wp.pl>
2023-12-28 17:08:32 +0100

    init2

--- src/App.js
+++ src/App.js
@@ -1,19 +1,103 @@
 import {useState} from "react";
 
+
+function* genId(){
+    let id = 0;
+
+    while(true){
+
+        yield id
+        id++
+    }
+
+}
+
+const nextId = genId()
+
 function App() {
 
     const [value, setValue] = useState('')
+    const [tasks, setTasks] = useState([])
+    const [filter, setFilter] = useState('all')
 
-    function handleInput(){
 
+
+    function handleInput(event){
+      setValue(event.target.value)
 
     }
+
+
+    function handleAddTask(event){
+        if(event.key === 'Enter'){
+        setTasks([...tasks, {
+            id: nextId.next().value,
+            name: value,
+            status: false
+        }])
+        setValue('')
+
+        }
+
+    }
+
+    function handleChangeStatus(task){
+     task.status = !task.status
+        setTasks([...tasks])
+
+    }
+
+    function handleDeleteTask(taskToRemove){
+        setTasks(tasks.filter((task) => task !== taskToRemove))
+
+    }
+
+
+    function handleDeleteAllTasks(){
+        setTasks(tasks.filter((task) => !task.status))
+
+    }
+
+
+    function handleAllDone(){
+        let done =  tasks.every((task) => task.status === true)
+
+        setTasks(tasks.map((task) => ({...task, status: !done})))
+
+    }
+
+
 
   return (
     <div>
     <h1>todos</h1>
         <div>
-            <input type="text" value={value} placeholder='What needs to be done?'/>
+            <button onClick={handleAllDone}>Done All</button>
+
+            <input type="text" onKeyUp={handleAddTask} onChange={handleInput} value={value} placeholder='What needs to be done?'/>
+            <ul>
+                {tasks
+                    .filter((task) => filter === 'all' ? true : task.status === filter)
+                    .map((task) => (
+                    <li key={task.id}>
+                        <button onClick={() =>  {handleChangeStatus(task)}}>{`${task.status}`}</button>
+                        <span>{task.name}</span>
+                        <button onClick={() =>  {handleDeleteTask(task)}}>x</button>
+                    </li>))}
+            </ul>
+
+            <span>{tasks.filter((task) => !task.status).length} items left</span>
+
+
+            <div>
+                <button onClick={() => setFilter('all')}>All</button>
+                <button onClick={() => setFilter(false)}>Active</button>
+                <button onClick={() => setFilter(true)}>Completed</button>
+            </div>
+
+
+            {!!tasks.filter((task) => task.status).length  && <button onClick={handleDeleteAllTasks}>Clear Completed</button>}
+
         </div>
     </div>
   );
